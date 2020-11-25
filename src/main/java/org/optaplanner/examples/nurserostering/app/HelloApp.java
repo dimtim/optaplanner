4b360ab72e86c5ce65089013c814e876d4ef72d9
Dmitry.Timakin <Dmitry.Timakin@vineyard.ru>
2020-11-25 17:40:28 +0300

    Fix

--- src/main/java/org/optaplanner/examples/nurserostering/app/HelloApp.java
+++ src/main/java/org/optaplanner/examples/nurserostering/app/HelloApp.java
@@ -11,37 +11,37 @@ import org.optaplanner.examples.nurserostering.persistence.NurseRosteringExporte
 import org.optaplanner.examples.nurserostering.persistence.NurseRosteringImporter;
 
 public class HelloApp {
-	public static final String SOLVER_CONFIG_XML = "org/optaplanner/examples/nurserostering/solver/nurseRosteringSolverConfig.xml";
-	public static ScoreDirector scoreDirector;
-	public static int printCount = 0;
+    public static final String SOLVER_CONFIG_XML = "org/optaplanner/examples/nurserostering/solver/nurseRosteringSolverConfig.xml";
+    public static ScoreDirector scoreDirector;
+    public static int printCount = 0;
 
-	public static void main(String[] args) {
-		Solver solver = SolverFactory.createFromXmlResource(SOLVER_CONFIG_XML)
-				.buildSolver();
-		ScoreDirectorFactory scoreDirectorFactory = solver
-				.getScoreDirectorFactory();
-		scoreDirector = scoreDirectorFactory.buildScoreDirector();
+    public static void main(String[] args) {
+        Solver solver = SolverFactory.createFromXmlResource(SOLVER_CONFIG_XML)
+                .buildSolver();
+        ScoreDirectorFactory scoreDirectorFactory = solver
+                .getScoreDirectorFactory();
+        scoreDirector = scoreDirectorFactory.buildScoreDirector();
 
-		NurseRosteringImporter importer = new NurseRosteringImporter();
-		NurseRoster helloSolution = importer.readSolution(new File("/Users/admin/git/optaplannerschedules/data/nurserostering/import/toy2.xml"));
+        NurseRosteringImporter importer = new NurseRosteringImporter();
+        NurseRoster helloSolution = importer.readSolution(new File("/Users/admin/git/optaplannerschedules/data/nurserostering/import/toy2.xml"));
 
-		scoreDirector.setWorkingSolution(helloSolution);
-		System.out.println("\n\nTRYING COUNT AT:");
-		solver.solve(helloSolution);
-		NurseRoster bestHelloSolution = (NurseRoster) solver
-				.getBestSolution();
-		System.out.println(bestHelloSolution);
-		NurseRosteringExporter exporter = new NurseRosteringExporter();
-		exporter.writeSolution(bestHelloSolution, new File("/Users/admin/git/optaplannerschedules/data/nurserostering/import/toy2_solution.xml"));
-	}
+        scoreDirector.setWorkingSolution(helloSolution);
+        System.out.println("\n\nTRYING COUNT AT:");
+        solver.solve(helloSolution);
+        NurseRoster bestHelloSolution = (NurseRoster) solver
+                .getBestSolution();
+        System.out.println(bestHelloSolution);
+        NurseRosteringExporter exporter = new NurseRosteringExporter();
+        exporter.writeSolution(bestHelloSolution, new File("/Users/admin/git/optaplannerschedules/data/nurserostering/import/toy2_solution.xml"));
+    }
 
-	public static String getSpacerFeed() {
-		printCount++;
-		if (printCount == 25) {
-			printCount = 0;
-			return "\n";
-		} else {
-			return " ";
-		}
-	}
+    public static String getSpacerFeed() {
+        printCount++;
+        if (printCount == 25) {
+            printCount = 0;
+            return "\n";
+        } else {
+            return " ";
+        }
+    }
 }
